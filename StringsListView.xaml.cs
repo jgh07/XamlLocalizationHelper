d00989a312a5a709c6fdd6deb278ee794945e1e8
Jonas Glaser-Heiß <loschag@outlook.de>
2022-01-09 14:02:04 +0100

    added a list view for all strings

---
+++ StringsListView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace XamlLocalizationHelper
+{
+    /// <summary>
+    /// Interaktionslogik für StringsListView.xaml
+    /// </summary>
+    public partial class StringsListView : AdonisUI.Controls.AdonisWindow
+    {
+        public StringsListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
